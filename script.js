74869196adf2358b4e39a81df9b285e464039650
Daniel-ben-dr <abdelrahem633@gmail.com>
2025-06-02 08:07:25 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// This file can be used to add JavaScript functionalities later.
+// Currently, it's empty but essential for linking to the HTML file.
+
+// Example: A simple console log for developers
+console.log("مرحبًا بك في موقع إمبراطورية الشمس!");
+
+// You can add more complex JavaScript here as needed, e.g., for
+// smooth scrolling, form validation, or dynamic content loading.
